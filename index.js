9c5869ccbe3ff362febf4154bf749fe8082ff71d
chris <chris.mahfouz@hotmail.com>
2021-03-23 10:51:50 +0200

    completed assignment domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+const newParagraph=document.getElementById('text');
+console.log(newParagraph)
+newParagraph.innerHTML="This is really cool!"
+
+    
+})
